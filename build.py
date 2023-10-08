d69dfc3ee0afa32604196022a3e912d2b8d4a243
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-10-08 19:07:13 +0200

    Update build.py

--- build.py
+++ build.py
@@ -4,15 +4,17 @@ package_directory = "package"
 os.chdir(package_directory)
 
 while True:
-    answer = input("1: install library\n2: uninstall library\n3: build\n4: upload to test.pypi.org\n\n>")
+    answer = input("1: install library\n2: uninstall library\n3: build\n4: upload to test.pypi.org\n5: upload to pypi.org\n\n>")
 
     if answer == '1':
-        os.system("pip install .")
+        os.system("pip install chromaconsole")
     elif answer == '2':
         os.system("pip uninstall chromaconsole")
     elif answer == '3':
         os.system("py -m build")
     elif answer == '4':
         os.system("py -m twine upload --repository testpypi dist/*")
+    elif answer == '5':
+        os.system("py -m twine upload --repository pypi dist/*")
     else:
         print("Invalid choice. Please select a valid option.")
