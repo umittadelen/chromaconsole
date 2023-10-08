57b74731c96b193312d298884b341abab03f0e66
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-10-08 18:00:52 +0200

    Update build.py

--- build.py
+++ build.py
@@ -13,8 +13,6 @@ while True:
     elif answer == '3':
         os.system("py -m build")
     elif answer == '4':
-        os.system("twine upload --repository-url https://upload.pypi.org/legacy/ dist/* --username __token__ --password pypi-AgENdGVzdC5weXBpLm9yZwIkY2VhYTdjMGEtOTk5YS00MzljLWJmNTAtMjRkYzRjZjA2NDBiAAIVWzEsWyJjaHJvbWFjb25zb2xlIl1dAAIsWzIsWyJjZGMzMzhmZi1hNTIxLTRlZjktOGQ2NC1hZmQyNDhjNGE2NDciXV0AAAYgxPagyMtLSck6e0G-87T7IMYeLWkhPcqyNGGOOPJh294 --verbose")
-    elif answer == '5':
         os.system("py -m twine upload --repository chromaconsole dist/*")
     else:
         print("Invalid choice. Please select a valid option.")
