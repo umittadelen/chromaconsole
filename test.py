e913fad9b6d7b03ca690bf388a29287a1936f280
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-10-10 12:36:31 +0200

    sum

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ text = (
     f"{Color.text(255, 0, 0)}Red Text{Style.reset()}\n"
     f"{Color.text(0, 255, 0)}Green Text{Style.reset()}\n"
     f"{Color.text(0, 0, 255)}Blue Text{Style.reset()}\n"
-    f"{Color.text('#FF00FF')}Magenta Text{Style.reset()}\n"
+    f"{Color.text('#F0F')}Magenta Text{Style.reset()}\n"
     f"{Color.background(255, 255, 0)}{Style.bold()}{Color.text(0, 0, 255)}Yellow Background{Style.reset()}\n"
-    f"{Color.background('#00FFFF')}Cyan Background{Style.reset()}\n"
+    f"{Color.background('#0FF')}Cyan Background{Style.reset()}\n"
     f"normal Text\n"
     f"{Style.bold()}Bold Text{Style.reset()}\n"
     f"{Style.italic()}Italic Text{Style.reset()}\n"
@@ -23,9 +23,9 @@ text = (
     f"{Color.text(0, 255, 255)}Cyan Text{Style.reset()}\n"
     f"{Color.text('#f2046f')}{Style.bold()}bold {Style.reset()}{Color.text('#f2046f')}{Style.italic()}italic {Style.bold()}both{Style.reset()}\n"
     f"{Color.text(200, 100, 100)}A{Style.bold()}A{Style.reset()}{Style.italic()}{Color.text(200, 100, 100)}A{Style.bold()}A{Style.reset()}\n"
-    f"{Style.bold()}{Color.background('#ffffff')}{Color.text('#000000')}white bg black bold text{Style.reset()}\n"
+    f"{Style.bold()}{Color.background('#fff')}{Color.text('#000')}white bg black bold text{Style.reset()}\n"
     f"{Style.minecraft('§','§ahello §4world§r')}\n"
-    f"{Color.text('#ffaa00')}text {Style.negative()}text\n{Style.reset()}"
+    f"{Color.text('#fa0')}text {Style.negative()}text\n{Style.reset()}"
 )
 
 print(text)
