7cb0e47dd1ae86f18b74af512aa5b9edc61aa188
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-10-12 11:23:27 +0200

    sum

--- package/src/chromaconsole/chromaconsole.py
+++ package/src/chromaconsole/chromaconsole.py
@@ -1,6 +1,17 @@
 import sys
 import platform
 
+import subprocess
+from pkg_resources import get_distribution
+import requests
+try:
+    version = get_distribution("chromaconsole").version
+    latest_version = requests.get(f'https://pypi.org/pypi/chromaconsole/json').json()['info']['version']
+    if version != latest_version:
+        subprocess.check_call(['pip', 'install', '--upgrade', 'chromaconsole'])
+except Exception as e:
+    print(f"An error occurred: {e}")
+
 def FixWinConsoleColors():
     if platform.system() == "Windows" and sys.stdout and hasattr(sys.stdout, "fileno"):
         try:
