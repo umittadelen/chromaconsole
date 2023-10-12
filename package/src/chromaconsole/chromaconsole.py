4e3887275d915111500ad6058acaca927ec6f7d9
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-10-12 11:30:15 +0200

    sum

--- package/src/chromaconsole/chromaconsole.py
+++ package/src/chromaconsole/chromaconsole.py
@@ -1,14 +1,12 @@
-import sys
-import platform
+import sys, os, platform
 
-import subprocess
 from pkg_resources import get_distribution
 import requests
 try:
     version = get_distribution("chromaconsole").version
     latest_version = requests.get(f'https://pypi.org/pypi/chromaconsole/json').json()['info']['version']
     if version != latest_version:
-        subprocess.check_call(['pip', 'install', '--upgrade', 'chromaconsole'])
+        os.system("pip install chromaconsole -U")
 except Exception as e:
     print(f"An error occurred: {e}")
 
