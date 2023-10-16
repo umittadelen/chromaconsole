ea69371a2b6d4fa47cfa694f18bba8c732c69c45
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-10-16 09:59:47 +0200

    Update bit.py

--- bit.py
+++ bit.py
@@ -1,13 +1,14 @@
 from chromaconsole import Color
 
+steps = 32
+
 def generate_32bit_color_table():
     color_table = ""
-    for r in range(0, 256, 32):
+    for r in range(0, 256, steps):
         row = ""
-        for g in range(0, 256, 32):
-            for b in range(0, 256, 32):
-                # Create a block of text with the current color
-                colored_block = Color.text(r, g, b) + "█" * 3
+        for g in range(0, 256, steps):
+            for b in range(0, 256, steps):
+                colored_block = Color.text(r, g, b) + "██"
                 row += colored_block
             row += "\033[0m"  # Reset the color at the end of each row
         color_table += row + "\n"  # Start a new row
