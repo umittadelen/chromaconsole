155684472537e93308880981d30f40e3062eeebc
umittadelen <72702832+umit-09@users.noreply.github.com>
2023-11-04 14:20:36 +0100

    sum

---
+++ package/src/chromaconsole/Color.py
@@ -0,0 +1,186 @@
+from Style import enabled
+
+class Color:
+    @staticmethod
+    def text(*args): #38;2;r;g;bm
+        '''
+        changes the color of the text\n
+        Color.text(r, g, b)\n
+        Color.text("#rrggbb")
+        '''
+        if enabled:
+            if len(args) == 1:
+                color = args[0].lstrip("#")
+                if len(color) == 3:
+                    r, g, b = (int(c * 2, 16) for c in color)
+                elif len(color) == 6:
+                    r, g, b = (int(color[i:i+2], 16) for i in range(0, 6, 2))
+                else:
+                    raise ValueError("Invalid color format")
+            elif len(args) == 3:
+                r, g, b = args
+            else:
+                raise ValueError("Invalid number of arguments")
+
+            return f"\033[38;2;{r};{g};{b}m"
+        else:
+            return ""
+        
+    @staticmethod
+    def default_text(): #39m
+        '''
+        Default foreground color\n
+        Implementation defined (according to standard)
+        '''
+        if enabled:
+            return "\033[39m"
+        else:
+            return ""
+    
+    @staticmethod
+    def background(*args): #48;2;r;g;bm
+        '''
+        changes the color of the text background\n
+        Color.background(r, g, b)\n
+        Color.background("#rrggbb")
+        '''
+        if enabled:
+            if len(args) == 1:
+                color = args[0].lstrip("#")
+                if len(color) == 3:
+                    r, g, b = (int(c * 2, 16) for c in color)
+                elif len(color) == 6:
+                    r, g, b = (int(color[i:i+2], 16) for i in range(0, 6, 2))
+                else:
+                    raise ValueError("Invalid color format")
+            elif len(args) == 3:
+                r, g, b = args
+            else:
+                raise ValueError("Invalid number of arguments")
+
+            return f"\033[48;2;{r};{g};{b}m"
+        else:
+            return ""
+    
+    @staticmethod
+    def default_background(): #49m
+        '''
+        Default background color\n
+        Implementation defined (according to standard)
+        '''
+        if enabled:
+            return "\033[49m"
+        else:
+            return ""
+    
+    @staticmethod
+    def text_black():
+        if enabled:
+            return "\033[30m"
+        else:
+            return ""
+        
+    @staticmethod
+    def text_red():
+        if enabled:
+            return "\033[31m"
+        else:
+            return ""
+        
+    @staticmethod
+    def text_green():
+        if enabled:
+            return "\033[32m"
+        else:
+            return ""
+        
+    @staticmethod
+    def text_yellow():
+        if enabled:
+            return "\033[33m"
+        else:
+            return ""
+        
+    @staticmethod
+    def text_blue():
+        if enabled:
+            return "\033[34m"
+        else:
+            return ""
+        
+    @staticmethod
+    def text_magenta():
+        if enabled:
+            return "\033[35m"
+        else:
+            return ""
+        
+    @staticmethod
+    def text_cyan():
+        if enabled:
+            return "\033[36m"
+        else:
+            return ""
+    
+    @staticmethod
+    def text_white():
+        if enabled:
+            return "\033[37m"
+        else:
+            return ""
+        
+    @staticmethod
+    def bg_black():
+        if enabled:
+            return "\033[40m"
+        else:
+            return ""
+        
+    @staticmethod
+    def bg_red():
+        if enabled:
+            return "\033[41m"
+        else:
+            return ""
+        
+    @staticmethod
+    def bg_green():
+        if enabled:
+            return "\033[42m"
+        else:
+            return ""
+        
+    @staticmethod
+    def bg_yellow():
+        if enabled:
+            return "\033[43m"
+        else:
+            return ""
+        
+    @staticmethod
+    def bg_blue():
+        if enabled:
+            return "\033[44m"
+        else:
+            return ""
+        
+    @staticmethod
+    def bg_magenta():
+        if enabled:
+            return "\033[45m"
+        else:
+            return ""
+        
+    @staticmethod
+    def bg_cyan():
+        if enabled:
+            return "\033[46m"
+        else:
+            return ""
+    
+    @staticmethod
+    def bg_white():
+        if enabled:
+            return "\033[47m"
+        else:
+            return ""
--- package/src/chromaconsole/__init__.py
+++ package/src/chromaconsole/__init__.py
@@ -1,1 +1,32 @@
-from .chromaconsole import Color, Style
+#https://en.wikipedia.org/wiki/ANSI_escape_code
+
+from .Color import Color
+from .Style import Style
+from .Console import Console
+
+__all__ = ['Color', 'Style', 'Console']
+
+try:
+    import sys, os, platform
+
+    from pkg_resources import get_distribution
+    try:
+        import requests
+        version = get_distribution("chromaconsole").version
+        latest_version = requests.get(f'https://pypi.org/pypi/chromaconsole/json').json()['info']['version']
+        if version != latest_version:
+            os.system("pip install chromaconsole -U")
+    except:
+        pass
+
+    def FixWinConsoleColors():
+        if platform.system() == "Windows" and sys.stdout and hasattr(sys.stdout, "fileno"):
+            try:
+                import ctypes
+                ctypes.windll.kernel32.SetConsoleMode(ctypes.windll.kernel32.GetStdHandle(-11), 7)
+            except Exception:
+                pass
+
+    FixWinConsoleColors()
+except:
+    pass
